4ab93aa8f82db2babf158a8c151650de73afea13
Production Team <production@deltamaker.com>
2024-03-01 00:11:07 +0000

    skr-pico reset/boot scripts

--- scripts/boot-skr-pico.sh
+++ scripts/boot-skr-pico.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Wiring GPIO26 connected to SKR Pico RESET pin,
+# Wiring GPIO26 connected to SKR Pico RESET pin, and
 #  GPIO19 connected to SKR Pico BOOT jumper
 # Pull GPIO19 low and Toggle GPIO26 to set board to USB Boot mode.
+# Exit with GPIO19 and GPIO26 set to high
 raspi-gpio set 19 op
 raspi-gpio set 19 dl
 raspi-gpio set 26 op
 raspi-gpio set 26 dl
-raspi-gpio set 26 ip
+raspi-gpio set 26 dh  # was ip
 sleep 0.5
-raspi-gpio set 19 ip
+raspi-gpio set 19 dh  # was ip
 sleep 1
 
--- scripts/reset-skr-pico.sh
+++ scripts/reset-skr-pico.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Wiring GPIO26 connected to SKR Pico RESET pin, and
+#  GPIO19 connected to SKR Pico BOOT jumper
 # Toggle GPIO26 to reset the SKR Pico board
+# Exit with GPIO19 and GPIO26 set to high
 raspi-gpio set 26 op
 raspi-gpio set 26 dl
-raspi-gpio set 26 ip
+raspi-gpio set 26 dh  # was ip
+raspi-gpio set 19 op
+raspi-gpio set 19 dh
 sleep 1
-
